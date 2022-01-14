dcef78442cef7f1f31a6db03baae1e71a1da5f43
Nayoung Kim <mwmstinc@gmail.com>
2022-01-14 19:20:40 +0900

    Works page csv to json complete

--- src/pages/works.js
+++ src/pages/works.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "../components/header";
+import { readString } from 'react-papaparse'
+import csvfile from './works/workslist.csv';
+import styles from './styles/Works.module.css';
 
-const Work =()=>{
-	return(
-		<Header title="Works"></Header>
-	);
-};
+class Work extends Component{
+	constructor(props){
+		super(props)
+	}
+
+
+	render(){
+		
+		readString(csvfile, {
+			header : true,
+			download: true,
+			complete: function (input) {
+				console.log(input)
+			}
+		});
+
+	
+		return(
+			<>
+			<Header title="Works"></Header>
+			</>
+		);
+	}
+}
 
 export default Work;
